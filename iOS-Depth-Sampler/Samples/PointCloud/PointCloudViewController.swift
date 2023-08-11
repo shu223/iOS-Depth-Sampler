e87083483a4f3f2cc8ddf50230e6e96a1789551f
shu223 <shuichi0526@gmail.com>
2023-08-11 15:11:06 +0900

    Support 32-bit depth / disparity

--- iOS-Depth-Sampler/Samples/PointCloud/PointCloudViewController.swift
+++ iOS-Depth-Sampler/Samples/PointCloud/PointCloudViewController.swift
@@ -197,34 +197,45 @@ extension CGImage {
 
 extension CVPixelBuffer {
 
-    // デプスが16bitで得られていることを前提
     func depthValues() -> [Float32] {
         CVPixelBufferLockBaseAddress(self, CVPixelBufferLockFlags(rawValue: 0))
         let width = CVPixelBufferGetWidth(self)
         let height = CVPixelBufferGetHeight(self)
         var pixelData = [Float32](repeating: 0, count: Int(width * height))
         let baseAddress = CVPixelBufferGetBaseAddress(self)!
-        guard
-            CVPixelBufferGetPixelFormatType(self) == kCVPixelFormatType_DepthFloat16 ||
-                CVPixelBufferGetPixelFormatType(self) == kCVPixelFormatType_DisparityFloat16
-            else { fatalError() }
 
-        // Float16という型がない（Floatは32bit）ので、UInt16として読み出す
-        let data = UnsafeMutableBufferPointer<UInt16>(start: baseAddress.assumingMemoryBound(to: UInt16.self), count: width * height)
-        for yMap in 0 ..< height {
-            for index in 0 ..< width {
-                let baseAddressIndex = index + yMap * width
-                // UInt16として読みだした値をFloat32に変換する
-                var f16Pixel = data[baseAddressIndex]  // Read as UInt16
-                var f32Pixel = Float32(0.0)
-                var src = vImage_Buffer(data: &f16Pixel, height: 1, width: 1, rowBytes: 2)
-                var dst = vImage_Buffer(data: &f32Pixel, height: 1, width: 1, rowBytes: 4)
-                vImageConvert_Planar16FtoPlanarF(&src, &dst, 0)
-                
-                // Float32の配列に格納
-                pixelData[baseAddressIndex] = f32Pixel
+        switch CVPixelBufferGetPixelFormatType(self) {
+        case kCVPixelFormatType_DepthFloat16, kCVPixelFormatType_DisparityFloat16:
+            // Float16という型がない（Floatは32bit）ので、UInt16として読み出す
+            let data = UnsafeMutableBufferPointer<UInt16>(start: baseAddress.assumingMemoryBound(to: UInt16.self), count: width * height)
+            for yMap in 0 ..< height {
+                for index in 0 ..< width {
+                    // UInt16として読みだした値をFloat32に変換する
+                    let baseAddressIndex = index + yMap * width
+                    var f16Pixel = data[baseAddressIndex]  // Read as UInt16
+                    var f32Pixel = Float32(0.0)
+                    var src = vImage_Buffer(data: &f16Pixel, height: 1, width: 1, rowBytes: 2)
+                    var dst = vImage_Buffer(data: &f32Pixel, height: 1, width: 1, rowBytes: 4)
+                    vImageConvert_Planar16FtoPlanarF(&src, &dst, 0)
+
+                    // Float32の配列に格納
+                    pixelData[baseAddressIndex] = f32Pixel
+                }
             }
+
+        case kCVPixelFormatType_DepthFloat32, kCVPixelFormatType_DisparityFloat32:
+            let data = UnsafeMutableBufferPointer<Float32>(start: baseAddress.assumingMemoryBound(to: Float32.self), count: width * height)
+            for yMap in 0 ..< height {
+                for index in 0 ..< width {
+                    let baseAddressIndex = index + yMap * width
+                    pixelData[baseAddressIndex] = data[baseAddressIndex]
+                }
+            }
+
+        default:
+            fatalError("Unsupported pixel format")
         }
+
         CVPixelBufferUnlockBaseAddress(self, CVPixelBufferLockFlags(rawValue: 0))
         return pixelData
     }
